dce6fbbd9fa35078ae58e4cc487c7378c6c8a180
komour <komaroff404@gmail.com>
2020-11-24 16:54:21 +0300

    init

---
+++ code/attention-module/scripts/train_imagenet_resnet50_cbam.sh
@@ -0,0 +1,24 @@
+#python train_imagenet.py \
+#			--ngpu 8 \
+#			--workers 20 \
+#			--arch resnet --depth 50 \
+#			--epochs 100 \
+#			--batch-size 256 --lr 0.1 \
+#			--att-type CBAM \
+#			--prefix RESNET50_IMAGENET_CBAM \
+#			./data/ImageNet/
+#
+#
+
+
+python train_imagenet.py \
+			--ngpu 8 \
+			--workers 20 \
+			--arch resnet --depth 50 \
+			--epochs 100 \
+			--batch-size 256 --lr 0.1 \
+			--att-type CBAM \
+			--prefix RESNET50_IMAGENET_CBAM \
+			data/ISIC2018_Task1-2_Training_Input_10/
+
+#			--ngpu 8 --workers 20 --arch resnet --depth 50 --epochs 100 --batch-size 256 --lr 0.1 --att-type CBAM --prefix RESNET50_IMAGENET_CBAM data/ISIC2018_Task1-2_Training_Input_10/
