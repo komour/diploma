158a91caf93ecb9e4f5bc6e169b5c3843abdfa7e
komour <komaroff404@gmail.com>
2021-03-15 18:01:10 +0300

    Edit evaluation script

--- attention-module/evaluation_script.sh
+++ attention-module/evaluation_script.sh
@@ -1,1 +1,1 @@
-python3 train.py --tags concurrent baseline weighted --resume "checkpoints/RESNET50_CBAM_new_name_wrap.pth" --run-name "Mask instead SAM baseline" --lr 1e-4 --cuda-device 3 --number 0 --epochs 1000
+python3 train.py --tags concurrent baseline weighted --resume "checkpoints/RESNET50_CBAM_new_name_wrap.pth" --run-name "Mask instead SAM, 1000 epoch baseline" --lr 1e-4 --cuda-device 3 --number 0 --epochs 1000
