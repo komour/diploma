82ee6b56f7cde392fed510feb994457b8403c340
komour <komaroff404@gmail.com>
2021-03-30 21:36:02 +0300

    Add two more launches

--- attention-module/evaluation_script.sh
+++ attention-module/evaluation_script.sh
@@ -1,1 +1,3 @@
-python3 visualization.py --cuda-device 3 --resume "checkpoints/RESNET50_CBAM_new_name_wrap.pth" --vis-prefix "imagenet" --is-server 1 --run-name "vis-imagenet" --tags imagenet visualization
+python3 train.py --lr 1e-5 --cuda-device 3 --resume "checkpoints/outer-SAM-1_checkpoint.pth" --is-server 1 --run-name "1e-5seq1-outer-SAM-1" --tags outer-SAM-1 sequential-1 &&
+python3 train.py --lr 1e-5 --cuda-device 3 --resume "checkpoints/SAM-1_checkpoint.pth" --is-server 1 --run-name "1e-5seq1-SAM-1" --tags SAM-1 sequential-1
+
