f9dfbb6e1b0a98ed9dc5b6a217275f8a906f8e2f
komour <komaroff404@gmail.com>
2021-04-19 18:16:29 +0300

    Prepare runs

--- attention-module/multiple_executions.sh
+++ attention-module/multiple_executions.sh
@@ -1,8 +1,5 @@
-python3 train.py --lmbd 2 --arch BAM --lr 1e-4 --tags BAM SAM-3 concurrent --run-name "lmbd=2, SAM-3, lr=1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 --number 3 &&
-python3 train.py --lmbd 5 --arch BAM --lr 1e-4 --tags BAM SAM-3 concurrent --run-name "lmbd=5, SAM-3, lr=1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 --number 3 &&
-python3 train.py --lmbd 10 --arch BAM --lr 1e-4 --tags BAM SAM-3 concurrent --run-name "lmbd=10, SAM-3, lr=1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 --number 3 &&
+python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-2 concurrent --run-name "inv-SAM-2, lr=1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 --number -2 &&
+python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-2 concurrent outer --run-name "inv-outer-SAM-2, lr=1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 --number -20
 
-python3 train.py --lmbd 2 --arch BAM --lr 1e-4 --tags BAM SAM-3 concurrent outer --run-name "lmbd=2, outer-SAM-3, lr=1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 --number 30 &&
-python3 train.py --lmbd 5 --arch BAM --lr 1e-4 --tags BAM SAM-3 concurrent outer --run-name "lmbd=5, outer-SAM-3, lr=1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 --number 30 &&
-python3 train.py --lmbd 10 --arch BAM --lr 1e-4 --tags BAM SAM-3 concurrent outer --run-name "lmbd=10, outer-SAM-3, lr=1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 --number 30
-
+python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-2 concurrent --run-name "SAM-2, lr=1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 --number 2 &&
+python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-2 concurrent outer --run-name "outer-SAM-2, lr=1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 --number 20 &&
