4ba5bc909625b1aaafeb081cd60d72ee7e95d2d8
komour <komaroff404@gmail.com>
2021-04-20 10:03:21 +0300

    Update execution files

--- attention-module/multiple_executions2.sh
+++ attention-module/multiple_executions2.sh
@@ -1,5 +1,3 @@
-python3 train_copy.py --arch ResNet50 --lr 1e-4 --tags ResNet50 --run-name "ResNet50, lr=1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 3 &&
-python3 train_copy.py --arch ResNet50 --lr 1e-4 --tags ResNet50 --run-name "ResNet50, lr=1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 3 &&
-python3 train_copy.py --arch ResNet50 --lr 1e-4 --tags ResNet50 --run-name "ResNet50, lr=1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 3 &&
-python3 train_copy.py --arch ResNet50 --lr 1e-4 --tags ResNet50 --run-name "ResNet50, lr=1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 3 &&
-python3 train_copy.py --arch ResNet50 --lr 1e-4 --tags ResNet50 --run-name "ResNet50, lr=1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 3
+python3 train.py --arch BAM --lr 1e-4 --tags BAM baseline concurrent --run-name "baseline, lr=1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 1 --number 0 &&
+python3 train.py --arch BAM --lr 1e-4 --tags BAM baseline concurrent --run-name "baseline, lr=1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 1 --number 0
+
