342e356f2ebce957bf4ee6dda4cfb76145237771
komour <komaroff404@gmail.com>
2021-04-20 21:54:39 +0300

    Update evaluation script

--- attention-module/evaluation_script.sh
+++ attention-module/evaluation_script.sh
@@ -1,1 +1,2 @@
-python3 train.py --arch BAM --lr 1e-4 --tags BAM baseline concurrent --run-name "baseline512, lr=1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 1 --number 0
+python3 train.py --arch BAM --lr 1e-4 --tags BAM baseline concurrent --run-name "512baseline, lr=1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 1 --number 0 &&
+python3 train.py --arch BAM --lr 1e-4 --tags SAM-2 outer BAM concurrent --run-name "512outer-SAM-2, lr=1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 1 --number 20
