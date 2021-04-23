fea6aa2a6e66c0007901923fc395c0f4f05ca95c
komour <komaroff404@gmail.com>
2021-04-23 11:49:02 +0300

    Update execution files

--- attention-module/multiple_executions2.sh
+++ attention-module/multiple_executions2.sh
@@ -1,19 +1,19 @@
-python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-1 concurrent --run-name "SAM-1, lr = 1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 1 --number 1 &&
-python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-1 concurrent --run-name "SAM-1, lr = 1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 1 --number 1 &&
-python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-1 concurrent --run-name "SAM-1, lr = 1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 1 --number 1 &&
-python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-1 concurrent --run-name "SAM-1, lr = 1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 1 --number 1 &&
+python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-1 concurrent --run-name "SAM-1, lr = 1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 --number 1 &&
+python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-1 concurrent --run-name "SAM-1, lr = 1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 --number 1 &&
+python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-1 concurrent --run-name "SAM-1, lr = 1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 --number 1 &&
+python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-1 concurrent --run-name "SAM-1, lr = 1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 --number 1 &&
 
-python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-2 concurrent --run-name "SAM-2, lr = 1e-5" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 1 --number 2 &&
-python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-2 concurrent --run-name "SAM-2, lr = 1e-5" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 1 --number 2 &&
-python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-2 concurrent --run-name "SAM-2, lr = 1e-5" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 1 --number 2 &&
-python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-2 concurrent --run-name "SAM-2, lr = 1e-5" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 1 --number 2 &&
+python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-2 concurrent --run-name "SAM-2, lr = 1e-5" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 --number 2 &&
+python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-2 concurrent --run-name "SAM-2, lr = 1e-5" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 --number 2 &&
+python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-2 concurrent --run-name "SAM-2, lr = 1e-5" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 --number 2 &&
+python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-2 concurrent --run-name "SAM-2, lr = 1e-5" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 --number 2 &&
 
-python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-3 concurrent --run-name "SAM-3, lr = 1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 1 --number 3 &&
-python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-3 concurrent --run-name "SAM-3, lr = 1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 1 --number 3 &&
-python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-3 concurrent --run-name "SAM-3, lr = 1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 1 --number 3 &&
-python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-3 concurrent --run-name "SAM-3, lr = 1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 1 --number 3 &&
+python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-3 concurrent --run-name "SAM-3, lr = 1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 --number 3 &&
+python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-3 concurrent --run-name "SAM-3, lr = 1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 --number 3 &&
+python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-3 concurrent --run-name "SAM-3, lr = 1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 --number 3 &&
+python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-3 concurrent --run-name "SAM-3, lr = 1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 --number 3 &&
 
-python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-all concurrent --run-name "SAM-all, lr = 1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 1 --number 5 &&
-python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-all concurrent --run-name "SAM-all, lr = 1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 1 --number 5 &&
-python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-all concurrent --run-name "SAM-all, lr = 1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 1 --number 5 &&
-python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-all concurrent --run-name "SAM-all, lr = 1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 1 --number 5
+python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-all concurrent --run-name "SAM-all, lr = 1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 --number 5 &&
+python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-all concurrent --run-name "SAM-all, lr = 1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 --number 5 &&
+python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-all concurrent --run-name "SAM-all, lr = 1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 --number 5 &&
+python3 train.py --arch BAM --lr 1e-4 --tags BAM SAM-all concurrent --run-name "SAM-all, lr = 1e-4" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 --number 5
