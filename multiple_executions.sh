8609161de60278c0523086b8290bb9d8142eb054
komour <komaroff404@gmail.com>
2021-05-05 23:42:14 +0300

    Move execution files to the separate folder

--- multiple_executions.sh
+++
@@ -1,7 +0,0 @@
-python3 train.py --run-type baseline --arch BAM --tags BAM baseline concurrent --run-name "baseline,  aug-vol.2" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 &&
-python3 train.py --run-type baseline --arch BAM --tags BAM baseline concurrent --run-name "baseline,  aug-vol.2" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 &&
-python3 train.py --run-type baseline --arch BAM --tags BAM baseline concurrent --run-name "baseline,  aug-vol.2" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 &&
-
-python3 train.py --run-type outer-SAM-1 --arch BAM --tags BAM outer-SAM-1 concurrent --run-name "outer-SAM-1,  aug-vol.2" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 &&
-python3 train.py --run-type outer-SAM-1 --arch BAM --tags BAM outer-SAM-1 concurrent --run-name "outer-SAM-1,  aug-vol.2" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 &&
-python3 train.py --run-type outer-SAM-1 --arch BAM --tags BAM outer-SAM-1 concurrent --run-name "outer-SAM-1,  aug-vol.2" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2
--- multiple_executions2.sh
+++
@@ -1,7 +0,0 @@
-python3 train.py --run-type outer-SAM-2 --arch BAM --tags BAM outer-SAM-2 concurrent --run-name "outer-SAM-2, aug-vol.2" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 &&
-python3 train.py --run-type outer-SAM-2 --arch BAM --tags BAM outer-SAM-2 concurrent --run-name "outer-SAM-2, aug-vol.2" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 &&
-python3 train.py --run-type outer-SAM-2 --arch BAM --tags BAM outer-SAM-2 concurrent --run-name "outer-SAM-2, aug-vol.2" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 &&
-
-python3 train.py --run-type outer-SAM-3 --arch BAM --tags BAM outer-SAM-3 concurrent --run-name "outer-SAM-3, aug-vol.2" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 &&
-python3 train.py --run-type outer-SAM-3 --arch BAM --tags BAM outer-SAM-3 concurrent --run-name "outer-SAM-3, aug-vol.2" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2 &&
-python3 train.py --run-type outer-SAM-3 --arch BAM --tags BAM outer-SAM-3 concurrent --run-name "outer-SAM-3, aug-vol.2" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 2
