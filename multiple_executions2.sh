caf321e7fd52c2130c1ca739617cba29f8e9e8b2
komour <komaroff404@gmail.com>
2021-05-07 17:29:26 +0300

    Update execution files

--- multiple_executions2.sh
+++ multiple_executions2.sh
@@ -1,2 +1,2 @@
-python3 ../train.py --run-type outer-SAM-2 --arch ResNet18BAM --tags ResNet18BAM outer-SAM-2 concurrent --run-name "outer-SAM-2,  ResNet18" --cuda-device 1 &&
-python3 ../train.py --run-type outer-SAM-3 --arch ResNet18BAM --tags ResNet18BAM outer-SAM-3 concurrent --run-name "outer-SAM-3,  ResNet18" --cuda-device 1
+python3 train.py --run-type outer-SAM-2 --arch ResNet18BAM --tags ResNet18BAM outer-SAM-2 concurrent --run-name "outer-SAM-2,  ResNet18" --cuda-device 1 &&
+python3 train.py --run-type outer-SAM-3 --arch ResNet18BAM --tags ResNet18BAM outer-SAM-3 concurrent --run-name "outer-SAM-3,  ResNet18" --cuda-device 1
