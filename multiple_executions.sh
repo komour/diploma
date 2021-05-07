ec51a1929339a26f6e7ee784dfbf37a695de1fd1
komour <komaroff404@gmail.com>
2021-05-08 01:49:08 +0300

    Change CUDA device

--- multiple_executions.sh
+++ multiple_executions.sh
@@ -1,5 +1,5 @@
-python3 train.py --run-type baseline --arch ResNet18BAM --tags ResNet18BAM baseline concurrent --run-name "baseline,  ResNet18BAM-1" --cuda-device 1 &&
-python3 train.py --run-type baseline --arch ResNet18BAM --tags ResNet18BAM baseline concurrent --run-name "baseline,  ResNet18BAM-1" --cuda-device 1 &&
-python3 train.py --run-type baseline --arch ResNet18BAM --tags ResNet18BAM baseline concurrent --run-name "baseline,  ResNet18BAM-1" --cuda-device 1 &&
-python3 train.py --run-type baseline --arch ResNet18BAM --tags ResNet18BAM baseline concurrent --run-name "baseline,  ResNet18BAM-1" --cuda-device 1 &&
-python3 train.py --run-type baseline --arch ResNet18BAM --tags ResNet18BAM baseline concurrent --run-name "baseline,  ResNet18BAM-1" --cuda-device 1
+python3 train.py --run-type baseline --arch ResNet18BAM --tags ResNet18BAM baseline concurrent --run-name "baseline,  ResNet18BAM-1" --cuda-device 3 &&
+python3 train.py --run-type baseline --arch ResNet18BAM --tags ResNet18BAM baseline concurrent --run-name "baseline,  ResNet18BAM-1" --cuda-device 3 &&
+python3 train.py --run-type baseline --arch ResNet18BAM --tags ResNet18BAM baseline concurrent --run-name "baseline,  ResNet18BAM-1" --cuda-device 3 &&
+python3 train.py --run-type baseline --arch ResNet18BAM --tags ResNet18BAM baseline concurrent --run-name "baseline,  ResNet18BAM-1" --cuda-device 3 &&
+python3 train.py --run-type baseline --arch ResNet18BAM --tags ResNet18BAM baseline concurrent --run-name "baseline,  ResNet18BAM-1" --cuda-device 3
--- multiple_executions2.sh
+++ multiple_executions2.sh
@@ -1,5 +1,5 @@
-python3 train.py --run-type outer-SAM-1 --arch ResNet18BAM --tags ResNet18BAM outer-SAM-1 concurrent --run-name "outer-SAM-1,  ResNet18BAM-1" --cuda-device 1 &&
-python3 train.py --run-type outer-SAM-1 --arch ResNet18BAM --tags ResNet18BAM outer-SAM-1 concurrent --run-name "outer-SAM-1,  ResNet18BAM-1" --cuda-device 1 &&
-python3 train.py --run-type outer-SAM-1 --arch ResNet18BAM --tags ResNet18BAM outer-SAM-1 concurrent --run-name "outer-SAM-1,  ResNet18BAM-1" --cuda-device 1 &&
-python3 train.py --run-type outer-SAM-1 --arch ResNet18BAM --tags ResNet18BAM outer-SAM-1 concurrent --run-name "outer-SAM-1,  ResNet18BAM-1" --cuda-device 1 &&
-python3 train.py --run-type outer-SAM-1 --arch ResNet18BAM --tags ResNet18BAM outer-SAM-1 concurrent --run-name "outer-SAM-1,  ResNet18BAM-1" --cuda-device 1
+python3 train.py --run-type outer-SAM-1 --arch ResNet18BAM --tags ResNet18BAM outer-SAM-1 concurrent --run-name "outer-SAM-1,  ResNet18BAM-1" --cuda-device 3 &&
+python3 train.py --run-type outer-SAM-1 --arch ResNet18BAM --tags ResNet18BAM outer-SAM-1 concurrent --run-name "outer-SAM-1,  ResNet18BAM-1" --cuda-device 3 &&
+python3 train.py --run-type outer-SAM-1 --arch ResNet18BAM --tags ResNet18BAM outer-SAM-1 concurrent --run-name "outer-SAM-1,  ResNet18BAM-1" --cuda-device 3 &&
+python3 train.py --run-type outer-SAM-1 --arch ResNet18BAM --tags ResNet18BAM outer-SAM-1 concurrent --run-name "outer-SAM-1,  ResNet18BAM-1" --cuda-device 3 &&
+python3 train.py --run-type outer-SAM-1 --arch ResNet18BAM --tags ResNet18BAM outer-SAM-1 concurrent --run-name "outer-SAM-1,  ResNet18BAM-1" --cuda-device 3
