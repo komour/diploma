e890f4eb67806142da119a72fa8984e8806ce96f
komour <komaroff404@gmail.com>
2021-05-30 00:26:56 +0300

    Prepare run with ResNet18

--- multiple_executions.sh
+++ multiple_executions.sh
@@ -1,1 +1,1 @@
-python3 train.py --run-type baseline --arch BAM --tags BAM baseline concurrent --run-name "T * (C * S)" --resume checkpoints/RESNET50_IMAGENET_BAM_best.pth.tar --cuda-device 1
+python3 train.py --run-type baseline --arch ResNet18BAM --tags BAM baseline concurrent --run-name "ResNet18BAM" --cuda-device 2
