c6b0226f5f568c47f2589636b1d334c78b07b4fd
komour <komaroff404@gmail.com>
2021-05-30 18:20:03 +0300

    Add more runs

--- multiple_executions.sh
+++ multiple_executions.sh
@@ -1,2 +1,5 @@
-python3 train.py --run-type outer-SAM-all --arch ResNet18BAM --tags BAM outer concurrent --run-name "0+outer-BAM-all" --cuda-device 3 &&
-python3 train.py --run-type outer-SAM-1 --arch ResNet18BAM --tags BAM outer concurrent --run-name "0+outer-BAM-1" --cuda-device 3
+python3 train.py --run-type baseline --arch ResNet18BAM --tags BAM outer concurrent --run-name "0+baseline" --cuda-device 3 &&
+python3 train.py --run-type baseline --arch ResNet18BAM --tags BAM outer concurrent --run-name "0+baseline" --cuda-device 3 &&
+python3 train.py --run-type baseline --arch ResNet18BAM --tags BAM outer concurrent --run-name "0+baseline" --cuda-device 3 &&
+python3 train.py --run-type baseline --arch ResNet18BAM --tags BAM outer concurrent --run-name "0+baseline" --cuda-device 3 &&
+python3 train.py --run-type baseline --arch ResNet18BAM --tags BAM outer concurrent --run-name "0+baseline" --cuda-device 3
--- multiple_executions1.sh
+++ multiple_executions1.sh
@@ -1,1 +1,5 @@
-python3 train.py --run-type outer-SAM-3 --arch ResNet18BAM --tags BAM outer concurrent --run-name "0+outer-BAM-3" --cuda-device 3
+python3 train.py --run-type outer-SAM-3 --arch ResNet18BAM --tags BAM outer concurrent --run-name "0+outer-BAM-3" --cuda-device 2 &&
+python3 train.py --run-type outer-SAM-3 --arch ResNet18BAM --tags BAM outer concurrent --run-name "0+outer-BAM-3" --cuda-device 2 &&
+python3 train.py --run-type outer-SAM-3 --arch ResNet18BAM --tags BAM outer concurrent --run-name "0+outer-BAM-3" --cuda-device 2 &&
+python3 train.py --run-type outer-SAM-3 --arch ResNet18BAM --tags BAM outer concurrent --run-name "0+outer-BAM-3" --cuda-device 2 &&
+python3 train.py --run-type outer-SAM-3 --arch ResNet18BAM --tags BAM outer concurrent --run-name "0+outer-BAM-3" --cuda-device 2
