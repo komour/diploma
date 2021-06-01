91abbfd2670372ede2fa386424571ea564363a52
komour <komaroff404@gmail.com>
2021-06-01 18:32:17 +0300

    Add runs

--- multiple_executions.sh
+++ multiple_executions.sh
@@ -1,3 +1,3 @@
-python3 train.py --run-type baseline --arch ResNet18BAM --tags BAM outer concurrent --run-name "0+baseline" --cuda-device 2 &&
-python3 train.py --run-type baseline --arch ResNet18BAM --tags BAM outer concurrent --run-name "0+baseline" --cuda-device 2 &&
-python3 train.py --run-type baseline --arch ResNet18BAM --tags BAM outer concurrent --run-name "0+baseline" --cuda-device 2
+python3 train.py --run-type outer-SAM-3 --arch ResNet18BAM --tags BAM outer concurrent --run-name "0+outer-SAM-3" --cuda-device 2 &&
+python3 train.py --run-type outer-SAM-3 --arch ResNet18BAM --tags BAM outer concurrent --run-name "0+outer-SAM-3" --cuda-device 2 &&
+python3 train.py --run-type outer-SAM-3 --arch ResNet18BAM --tags BAM outer concurrent --run-name "0+outer-SAM-3" --cuda-device 2
